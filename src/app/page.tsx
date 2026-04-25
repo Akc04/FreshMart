6c0ca38b2eda74ca4f39effdba19a6ce7ecd085d
Akc04 <ajaykumarchoudharyakc@gmail.com>
2026-04-26 01:52:58 +0530

    fix: correct image filenames for Corn and Butter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ function HeroAnimations() {
           alt="Leafy Green"
         />
         <motion.img 
-          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Maize.png" 
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Ear%20of%20Corn.png" 
           className="contained-item"
           animate={bucketActive ? { x: 130, y: -100, rotate: 20, scale: 0.8 } : { x: 0, y: 0, rotate: 0, scale: 0.35 }}
           transition={spring}
@@ -119,7 +119,7 @@ function HeroAnimations() {
           alt="Egg"
         />
         <motion.img 
-          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Butter%20Plate.png" 
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Butter.png" 
           className="contained-item"
           animate={cartActive ? { x: 150, y: -190, rotate: -10, scale: 0.8 } : { x: 0, y: 0, rotate: 0, scale: 0.35 }}
           transition={spring}
