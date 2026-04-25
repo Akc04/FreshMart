f13f942f439532021aa804d64255b2399a700a5d
Akc04 <ajaykumarchoudharyakc@gmail.com>
2026-04-26 03:13:49 +0530

    fix: correct repository name in next.config.ts to fix deployment pathing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = process.env.REPO_NAME || 'grocery-app';
+const repoName = 'FreshMart';
 
 const nextConfig: NextConfig = {
   output: 'export',
